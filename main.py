ea8a038b59771fa8d2cd58ac459792a526c84a23
Erkinbek008 <amiroverkinbek008@gmail.com>
2022-11-06 14:54:30 +0500

    I added main.py file

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def malumot(fayl_nomi : str, eski_joy : str, yangi_joy : str):
-    pass
-def malumot2(eski_joy : str, yangi_joy : str):
-    pass
+from pathlib import Path
+def fayl(fayl_nomi : str, eski_joy : str, yangi_joy : str):
+
+    desktop =Path("/Users/Erkin/Desktop/")
+    eski_joy = desktop/eski_joy/fayl_nomi
+    yangi_joy = desktop/yangi_joy/fayl_nomi
+    eski_joy.replace(yangi_joy)
+fayl("TEXT","Olmaliq","Toshkent")
+def fayl2(eski_joy : str, yangi_joy : str):
+    desktop =Path("/Users/Erkin/Desktop/")
+    eski_joy =desktop/eski_joy/yangi_joy
+    yangi_joy =desktop/yangi_joy
+    eski_joy.replace(yangi_joy)
+    yangi_joy.replace(eski_joy)
+fayl2("Angren","Ohangaron")
